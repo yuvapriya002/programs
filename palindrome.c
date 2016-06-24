0211b022ef011c0fb93568a242af7245cf1ff640
yuvapriya002 <yuvapriya002@gmail.com>
2016-06-24 20:54:55 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int num,r,sum=0,temp;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    temp=num;
+    while(num){
+         r=num%10;
+         num=num/10;
+         sum=sum*10+r;
+    }
+    if(temp==sum)
+         printf("%d is a palindrome",temp);
+    else
+         printf("%d is not a palindrome",temp);
+    return 0;
+}
