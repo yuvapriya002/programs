a5914a5a3a123fe99c574ebc483ff4b79fabe876
yuvapriya002 <yuvapriya002@gmail.com>
2016-06-24 19:37:39 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+char c;
+ printf("Enter a character: ");
+ scanf("%c",&c);
+ if( (c>='a' && c<='z')||(c>='A' && c<='Z'))
+ printf("%c alphabet",c);
+ else
+ printf("%c not an alphabet",c);
+ return 0;
+}
